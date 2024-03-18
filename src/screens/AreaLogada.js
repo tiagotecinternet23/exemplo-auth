dfcffc03714a2ed72cc3b89f7c0a8248e69956c5
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-03-18 10:27:35 -0300

    Initial commit

---
+++ src/screens/AreaLogada.js
@@ -0,0 +1,30 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function AreaLogada() {
+  return (
+    <View style={estilos.container}>
+      <View style={estilos.topo}>
+        <Text style={estilos.bemVindo}>Bem-vindo(a)</Text>
+        <Button title="Logout" color="#D35400" />
+      </View>
+      <View style={estilos.geral}>
+        <Text>Você está na área logada.</Text>
+      </View>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FCF3CF",
+    padding: 16,
+  },
+  topo: {
+    marginVertical: 32,
+  },
+  bemVindo: {
+    fontSize: 24,
+    marginVertical: 16,
+  },
+});
